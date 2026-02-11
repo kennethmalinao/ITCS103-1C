077f43469d2d35e8ada6d0869b2bda46062e0e46
kennethmalinao <kennethmalinao2515@gmail.com>
2026-02-11 08:15:21 +0800

    MALINAO_HO2.py

--- MALINAO_HO2.py
+++ MALINAO_HO2.py
@@ -3,7 +3,7 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Kenneth's Profile")
 window.geometry("600x600")
-window.resizable(True,True)
+window.resizable(False,True)
 window.config(bg="pink")
 
 label=tk.Label(window, text ="Student Profile", font=("Arial", 32,"bold"), fg="black", bg="pink", anchor="s")
@@ -18,7 +18,8 @@ label=tk.Label(window, text="Birthday: June 25, 2005", font=("Arial", 14,"bold")
 label.pack(padx=(10), pady=(10), anchor="w")
 label=tk.Label(window, text="Motto:", font=("Arial", 14,"bold"), fg="black", bg="pink")
 label.pack(padx=(10), pady=(10), anchor="w")
-label=tk.Label(window, text="     Life is a gamble so play it wisely.", font=("Time New Roman", 14,"italic"), fg="black", bg="pink")
+label=tk.Label(window, text="     Life is a gamble so play it wisely.", font=("Times New Roman", 14,"italic"), fg="black", bg="pink")
 label.pack(anchor="w")
 
 window.mainloop()
+
